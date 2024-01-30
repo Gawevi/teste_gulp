5e6df39ccd086f771d4acb4979385bbb4cae19c4
Gawevi <gbielwade@gmail.com>
2024-01-30 19:35:23 -0300

    Commit inicial

---
+++ source/scripts/conta.js
@@ -0,0 +1,9 @@
+var numero1 = prompt("Digite o primeiro número:");
+var numero2 = prompt("Digite o segundo número:");
+
+numero1 = parseFloat(numero1);
+numero2 = parseFloat(numero2);
+
+var resultado = numero1 + numero2;
+
+console.log("A soma de " + numero1 + " e " + numero2 + " é: " + resultado);
